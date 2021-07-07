90c25e9d0c3f33c424da44ae4777030b94d46449
lucasmetron <lucasmetron@gmail.com>
2021-07-07 09:23:02 -0300

    redux separado

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const redux = require('redux');
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers; //função para combinar reducers
 
-const { incrementAction, decrementAction } = require('./actions/CounterActions');
-const { addItemAction } = require('./actions/listActions');
+const { incrementAction, decrementAction } = require('./actions/CounterActions'); //importando actions 
+const { addItemAction } = require('./actions/listActions');//importando actions
 
-const counterReducer = require('./reducers/CounterReducer');
-const listReducer = require('./reducers/ListReduce');
+const counterActions = require('./actions/CounterActions'); //tambem podemos importar da maneira tradicional
+const listActions = require('./actions/listActions');
+
+const counterReducer = require('./reducers/CounterReducer'); //importando reducers
+const listReducer = require('./reducers/ListReduce'); //importando reducers
 
 
 const allReducers = combineReducers({ //aqui onde é feito a comniação dos reducers
@@ -20,7 +23,7 @@ store.subscribe(() => { console.log(store.getState()) });
 
 store.dispatch(addItemAction('Lucas'));
 
-store.dispatch(incrementAction(10));
+store.dispatch(counterActions.incrementAction(20));
 store.dispatch(decrementAction(5));
 
 console.log("Meu nome é: " + store.getState().list[1])//expemplo
